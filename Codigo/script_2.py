6f9191909849c5df79bb42ec17b8af920b5fa2d5
Danilo Urias <d14urias@gmail.com>
2020-06-21 01:13:26 -0600

    Segundo script terminado

--- Codigo/script_2.py
+++ Codigo/script_2.py
@@ -1,21 +1,119 @@
+from datetime import datetime
 from pyspark import SparkContext
 import plotly.offline as py
 import plotly.graph_objs as go
 
 sc = SparkContext ("local","practica")
-texto= sc.textFile("C:/Archivos/ventas_video_juegos.csv")
+texto= sc.textFile("C:/Archivos/ventas.csv")
 
 rdd=texto.map(lambda linea:linea.split("|"))\
-.filter(lambda linea:(linea[4]!="Genre"))\
-.filter(lambda linea:(linea[4]!=""))\
-.filter(lambda linea:(linea[4]=="Action" or linea[4]=="Sports" or linea[4]=="Fighting" or linea[4]=="Shooter" or linea[4]=="Racing" or linea[4]=="Adventure" or linea[4]=="Strategy"))\
-.map(lambda linea:(linea[4],float(linea[10])))\
+.filter(lambda linea:(linea[0]!="Region"))\
+.filter(lambda linea:(linea[0]!=""))\
+.map(lambda linea:(linea[0],float(linea[11])))\
 .reduceByKey(lambda x,y:x+y)\
 .sortBy(lambda linea:linea[1],ascending=False)
 
 
-for categorias in rdd.collect():
-    print("Categoria: "+categorias[0]+"| Ventas globales: "+str(round(categorias[1],2)))
+for valor in rdd.collect():
+    print("Region: "+valor[0]+"| Total: "+str(round(valor[1],2)))
+
+etiquetas=[]
+valores=[]
+
+for elemento in rdd.collect():
+    etiquetas.append(elemento[0])
+    valores.append(elemento[1])
+
+fig = go.Figure(go.Pie(
+    labels=etiquetas,
+    values=valores,
+    text=etiquetas,
+    textposition='auto',
+))
+
+fig.update_layout(
+    title="Total de ingresos por región",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="#7f7f7f"
+    )
+)
+
+py.plot(figure_or_data=fig,filename="reporte2_a.html")
+
+##################### Segundo reporte ############################################
+
+rdd=texto.map(lambda linea:linea.split("|"))\
+.filter(lambda linea:(linea[1]=="Guatemala"))\
+.map(lambda linea:(str(datetime.strptime(linea[5], '%d/%m/%Y').date().year),int(linea[8])))\
+.reduceByKey(lambda x,y:x+y)\
+.sortBy(lambda linea:linea[1],ascending=False)
+
+
+for valor in rdd.collect():
+    print("Año: "+str(valor[0])+"| Unidades vendidas: "+str(valor[1]))
+print(rdd.collect())
+
+ejex=[]
+ejey=[]
+
+for elemento in rdd.collect():
+    ejex.append("["+elemento[0]+"]")
+    ejey.append(elemento[1])
+
+fig = go.Figure(go.Bar(
+    x=ejex,
+    y=ejey,
+    text=ejey,
+    textposition='auto',
+))
+
+fig.update_layout(
+    title="Año con más ventas en Guatemala",
+    xaxis_title="Años",
+    yaxis_title="Ventas",
+
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="#7f7f7f"
+    )
+)
+
+
+py.plot(figure_or_data=fig,filename="reporte2_b.html")
+
+
+##################### Tercer reporte ############################################
+
+ejex=[]
+ejey=[]
+
+rdd=texto.map(lambda linea:linea.split("|"))\
+.filter(lambda linea:(linea[3]=="Online"))\
+.filter(lambda linea:(linea[5]!="Order Date"))\
+.filter(lambda linea:(str(datetime.strptime(linea[5], '%d/%m/%Y').date().year)=="2010"))\
+.map(lambda linea:("Ingresos",float(linea[11])))\
+.reduceByKey(lambda x,y:x+y)+\
+\
+texto.map(lambda linea:linea.split("|"))\
+.filter(lambda linea:(linea[3]=="Online"))\
+.filter(lambda linea:(linea[5]!="Order Date"))\
+.filter(lambda linea:(str(datetime.strptime(linea[5], '%d/%m/%Y').date().year)=="2010"))\
+.map(lambda linea:("Costos",float(linea[12])))\
+.reduceByKey(lambda x,y:x+y)+\
+\
+texto.map(lambda linea:linea.split("|"))\
+.filter(lambda linea:(linea[3]=="Online"))\
+.filter(lambda linea:(linea[5]!="Order Date"))\
+.filter(lambda linea:(str(datetime.strptime(linea[5], '%d/%m/%Y').date().year)=="2010"))\
+.map(lambda linea:("Ganancia",float(linea[13])))\
+.reduceByKey(lambda x,y:x+y)
+
+
+for valor in rdd.collect():
+    print(str(valor[0])+": "+str(round(valor[1],2)))
 
 ejex=[]
 ejey=[]
@@ -32,13 +130,13 @@ fig = go.Figure(go.Bar(
 ))
 
 fig.update_layout(
-    title="Ventas globales",
-    xaxis_title="Categorias",
-    yaxis_title="Ventas",
+    title="Analisis de ingresos,costos,ganancias",
+
     font=dict(
         family="Courier New, monospace",
         size=18,
         color="#7f7f7f"
     )
 )
-py.plot(figure_or_data=fig,filename="reporte2_a.html")
+
+py.plot(figure_or_data=fig,filename="reporte2_c.html")
